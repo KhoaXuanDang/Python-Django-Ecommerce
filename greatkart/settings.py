24387a1f894ffff4b85a5c8f5db81257285b4d8c
Khoa Xuan Dang <dangkx@mail.uc.edu>
2024-04-18 02:28:58 -0400

    handled cart and checkout page for the logged in users

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a^bg0fh351y!8pc*%_^h^1d#g7)fd&nrass3ezel3wp99%e@*3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,11 +138,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+
+
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'your email address'
-EMAIL_HOST_PASSWORD = 'your app password'
+EMAIL_HOST_USER = config('USER_EMAIL')
+EMAIL_HOST_PASSWORD = config('EMAIL_APP_PASS')
 EMAIL_USE_TLS = True
 
 # Default primary key field type
