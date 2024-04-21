7b231073a538f0512bddca1bc9e0dc03c1a59bad
Khoa Xuan Dang <dangkx@mail.uc.edu>
2024-04-21 02:28:41 -0400

    product order and payments

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -27,4 +27,7 @@ urlpatterns = [
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
 
+    #ORDER
+    path('orders/', include('orders.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # getting image from app
